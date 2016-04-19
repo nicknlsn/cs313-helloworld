aaf850effcfee56675b961a111f600b3431bc627
Nick Nelson <nicknlsn@gmail.com>
2016-04-19 15:24:59 -0600

    more code

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
 		<title>yo diggy wiggy</title>
 	</head>
 
 	<body>
 		<h1>A Simple Hello World</h1>
-		<p>Hello!</p>
+		<p>Nick was here.</p>
 	</body>
 </html>
